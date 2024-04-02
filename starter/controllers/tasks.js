c23a1f32237d5948005d5d9e7527ec1fea2017ee
Shamma18 <118926699+Shamma18@users.noreply.github.com>
2024-04-02 21:42:35 +0530

    adding apis

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task=require('../models/task')
 
-const getAllTasks =(req,res)=>{
-    res.send('get all tasks')
+const getAllTasks =async(req,res)=>{
+   try{
+    const tasks =await Task.find({})
+    res.status(200).json({tasks})
+   }catch(error){
+
+   }
 }
 const createTask =async(req,res)=>{
     try{
@@ -12,15 +17,47 @@ const createTask =async(req,res)=>{
     }
    
 }
-const getTask =(req,res)=>{
-    // res.send('get single task ')
-    res.json({id:req.params.id})
+const getTask =async(req,res)=>{
+   
+    try{
+        const {id:taskID}= req.params
+        const task =await Task.findOne({_id:taskID })
+        if(!task){
+            return res.status(404).json({msg:`no task with id: ${taskID}`})
+        }
+        res.status(200).json({task })
+       
+       }catch(error){
+        res.status(500).json({msg:error})
+       }
 }
-const updateTask =(req,res)=>{
-    res.send('update task ')
+const updateTask =async (req,res)=>{
+   try{
+    const {id:taskID}= req.params
+    const task=await Task.findByIdAndUpdate({_id:taskID},req.body,{
+        new:true,runValidators:true 
+    })
+    if(!task){
+        return res.status(404).json({msg:`no task with id: ${taskID}`})
+    }
+    res.status(200).json({task})
+   }catch(error){ 
+     
+   }
 }
-const deleteTask =(req,res)=>{
-    res.send('delete task ')
+const deleteTask =async (req,res)=>{
+    try{
+        const {id:taskID}= req.params
+        const task =await Task.findOneAndDelete({_id:taskID })
+        if(!task){
+            return res.status(404).json({msg:`no task with id: ${taskID}`})
+        }
+        // res.status(200).json({task })
+        res.status(200).send()
+       
+       }catch(error){
+        res.status(500).json({msg:error})
+       }
 }
 module.exports={
     getAllTasks,createTask,getTask,updateTask,deleteTask
